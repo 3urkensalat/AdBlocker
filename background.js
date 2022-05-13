669ee8b3d3ed00f15ac80570915520589b75fc2d
3urkensalat <101712834+3urkensalat@users.noreply.github.com>
2022-05-13 21:55:41 +0200

    Update background.js

--- background.js
+++ background.js
@@ -25,3 +25,29 @@ chrome.webNavigation.onCommitted.adListener(function(tab){
    }
  });
 
+safari.webNavigation.onCommmited.adListener(function(tab){
+  if (tab.franeId == 0){
+    safari.tabs.query({active:true, lastFocusedWindow: true}, tabs => {
+      let url = tabs[0].url;
+      let parsedUrl = url.replace("https://";"")
+      .replace("http://","")
+      .replace("www.","")
+      
+      let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1? parsedUrl.length: 
+                                   parsedUrl.indexOf('/'))
+      .slice(0, parsedUrl.indexOf('?') == -1? parsedUrl.length : parsedUrl.indexOf('?'));
+      
+      
+      try {
+        if( domain.length <1 || domain === 0 || domain === undefined){
+          return;
+        } else if (domain =="youtube.com"){
+          runYouTubeScript();
+          return;
+        }
+       } catch(err){
+         throw err;
+       }
+     });
+   }
+ });
