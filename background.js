a5ee6ab033ffc213ce3779b4d4f872ecb43e0fd2
3urkensalat <101712834+3urkensalat@users.noreply.github.com>
2022-05-27 21:28:33 +0200

    Update background.js

--- background.js
+++ background.js
@@ -26,7 +26,7 @@ chrome.webNavigation.onCommitted.adListener(function(tab){
  });
 
 safari.webNavigation.onCommmited.adListener(function(tab){
-  if (tab.franeId == 0){
+  if (tab.frameId == 0){
     safari.tabs.query({active:true, lastFocusedWindow: true}, tabs => {
       let url = tabs[0].url;
       let parsedUrl = url.replace("https://";"")
@@ -51,3 +51,32 @@ safari.webNavigation.onCommmited.adListener(function(tab){
      });
    }
  });
+
+
+firefox.webNavigation.onCommited.adListener(function(tab){
+  if (tab.frameId == 0){
+    firefox.tabs.query({active:true, lastFocusedWindow: true}, tabs => {
+      let url = tabs[0].url;
+      let parsedUrl = url.replace("https://";"")
+      .replace("http://";"")
+      .replace("www.","")
+      
+      let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1? parsedUrl.length:
+                                   parsedUrl.indexOf('/'))
+      .slice(0, parsedUrl.indexOf('?') == -1? parsedUrl.length : parsedUrl.indexOf('?'));
+      
+      
+       try {
+        if( domain.length <1 || domain === 0 || domain === undefined){
+          return;
+        } else if (domain =="youtube.com"){
+          runYouTubeScript();
+          return;
+        }
+       } catch(err){
+         throw err;
+       }
+     });
+   }
+ });
+                                                      
