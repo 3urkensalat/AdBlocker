38c2846ea127043192f001ad5ab09b5cc367ccd2
3urkensalat <101712834+3urkensalat@users.noreply.github.com>
2022-06-06 21:49:59 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,6 +1,6 @@
 function removeAds(){
   
-  let spans.getElementsByTagName("Werbung);
+  let spans.getElementsByTagName("Werbung");
   for (let i = 0; i < spans.length; ++i){
     if(spans[i].innerHTML === "Promoted"){
       let card = spans[i].closest(".feed-shared-update-v2");
@@ -22,3 +22,86 @@ removeAds();
 setInterval(function() {
   removeAds();
 },120)
+
+
+
+function removeAds(){
+  
+  let spans.getElementsByTagName("span");
+  for (let i = 0; i < spans.length; ++i){
+    if(spans[i].innerHTML === "Promoted"){
+      let card = spans[i].closest(".feed-shared-update-v2");
+      if (card === null){
+        let j = 0;
+        card = spans[i];
+        while (j < 7){
+          card = card.parentNode;
+          ++j;
+        }
+      }
+      card.setAttribute("style", "display: none !important;");
+    }
+  }
+}
+
+removeAds();
+
+setInterval(function() {
+  removeAds();
+},120)
+
+
+
+function removeAds(){
+  
+  let spans.getElementsByTagName("Werbung");
+  for (let i = 0; i < spans.length; ++i){
+    if(spans[i].innerHTML === "Promoted"){
+      let card = spans[i].closest(".feed-shared-update-v2");
+      if (card === null){
+        let j = 0;
+        card = spans[i];
+        while (j < 7){
+          card = card.parentNode;
+          ++j;
+        }
+      }
+      card.setAttribute("style", "display: none !important;");
+    }
+  }
+}
+
+removeAds();
+
+setInterval(function() {
+  removeAds();
+},12)
+
+
+
+
+
+function removeAds(){
+  
+  let spans.getElementsByTagName("span");
+  for (let i = 0; i < spans.length; ++i){
+    if(spans[i].innerHTML === "Promoted"){
+      let card = spans[i].closest(".feed-shared-update-v2");
+      if (card === null){
+        let j = 0;
+        card = spans[i];
+        while (j < 7){
+          card = card.parentNode;
+          ++j;
+        }
+      }
+      card.setAttribute("style", "display: none !important;");
+    }
+  }
+}
+
+removeAds();
+
+setInterval(function() {
+  removeAds();
+},11)
